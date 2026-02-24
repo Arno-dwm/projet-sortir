b96a72d407bd16fcf37852c518f389a326b085f6
1316-dev <mathieu.pineau@hotmail.fr>
2026-02-24 15:18:08 +0100

    inscription et desinscription

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Inscription;
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Repository\InscriptionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -69,6 +71,44 @@ final class SortieController extends AbstractController
             'sortie' => $sortie,
         ]);
 
+    }
+
+    #[Route('/inscription/{id}', name: '_inscription', requirements:['id' => '\d+'])]
+    public function inscrption(Sortie $sortie, EntityManagerInterface $em): Response
+    {
+        $inscription = new Inscription();
+        $inscription->setSortie($sortie);
+        $inscription->setDateInscription(new \DateTime('now'));
+        $inscription->setParticipant($this->getUser());
+        $em->persist($inscription);
+        $em->flush();
+
+        $this->addFlash('success', "Votre inscription à la sortie {$sortie->getNom()} a été enregistrée");
+
+
+        return $this->redirectToRoute('app_sortie_detail', ['id' => $sortie->getId()]);
+
+    }
+    #[Route('/desinscription/{id}', name: '_desinscription', requirements:['id' => '\d+'])]
+    public function desinscrire(Sortie $sortie, EntityManagerInterface $em, InscriptionRepository $insRepo, Request $request): Response
+    {
+        $inscription = $insRepo->findOneBy(['sortie' => $sortie, 'participant' => $this->getUser()]);
+        $token = $request->query->get('_token');
+
+        if ($this->isCsrfTokenValid('inscription_delete' . $sortie->getId(), $token)) {
+
+
+            $em->remove($inscription);
+            $em->flush();
+
+            $this->addFlash('success', "Votre inscription à la sortie {$sortie->getNom()} a été supprimé");
+
+            return $this->redirectToRoute('app_sortie_detail', ['id' => $sortie->getId()]);
+        }
+
+        $this->addFlash('danger', 'Cette action est impossible !');
+
+        return $this->redirectToRoute('app_sortie_detail', ['id' => $sortie->getId()]);
     }
 
 }
