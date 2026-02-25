c5b77a316ce5f715d4ca935afd10db04d6294ac7
1316-dev <mathieu.pineau@hotmail.fr>
2026-02-25 14:34:01 +0100

    sauvegarde sql evenement

--- data/evenement-sql.sql
+++ data/evenement-sql.sql
@@ -1,9 +1,11 @@
+#-----------------------------------------------------
 # PASSEE à l'état CLO : inscriptions cloturé
+#-----------------------------------------------------
 UPDATE sortie
 SET etat_id	= (SELECT id FROM etat WHERE code = 'CLO' LIMIT 1)
 WHERE (NOW() BETWEEN date_limite_inscription and date_heure_debut);
 
-
+#  Pas utilisé, gérer directement au moment de l'inscription
 UPDATE sortie s
     INNER JOIN (
         SELECT sortie_id, COUNT(*) as total
@@ -13,19 +15,30 @@ UPDATE sortie s
 SET s.etat_id = (SELECT id FROM etat WHERE code = 'CLO' LIMIT 1)
 WHERE i.total >= s.nb_inscriptions_max;
 
-
+#-----------------------------------------------------
 # PASSEE à l'état EC : activité en cours
+#-----------------------------------------------------
 UPDATE sortie
 SET etat_id	= (SELECT id FROM etat WHERE code = 'EC' LIMIT 1)
 WHERE NOW() BETWEEN  date_heure_debut and DATE_ADD(date_heure_debut, INTERVAL duree MINUTE);
 
+#-----------------------------------------------------
+# PASSEE à l'état FIN : Activité terminées
+#-----------------------------------------------------
+UPDATE sortie
+SET etat_id	= (SELECT id FROM etat WHERE code = 'FIN' LIMIT 1)
+WHERE NOW() > DATE_ADD(date_heure_debut, INTERVAL duree MINUTE);
 
-# PASSE à l'état OUV : inscription ouverte
+
+#-----------------------------------------------------
+# PASSE à l'état OUV : inscription ouverte : Pas besoin
+#-----------------------------------------------------
 
 UPDATE sortie
 SET etat_id	= (SELECT id FROM etat WHERE code = 'OUV' LIMIT 1)
 WHERE date_limite_inscription > NOW();
 
+#  Pas utilisé, gérer directement au moment de l'inscription
 UPDATE sortie s
     INNER JOIN (
         SELECT sortie_id, COUNT(*) as total
@@ -35,7 +48,3 @@ UPDATE sortie s
 SET s.etat_id = (SELECT id FROM etat WHERE code = 'OUV' LIMIT 1)
 WHERE i.total < s.nb_inscriptions_max;
 
-# PASSEE à l'état FIN : Activité terminées
-UPDATE sortie
-SET etat_id	= (SELECT id FROM etat WHERE code = 'FIN' LIMIT 1)
-WHERE NOW() > DATE_ADD(date_heure_debut, INTERVAL duree MINUTE);
