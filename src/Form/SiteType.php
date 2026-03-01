3719ec7c9812daa7241f447f5e635027cb6757c0
lenalec44 <helena.lecable@gmail.com>
2026-03-01 14:57:51 +0100

    Gestion sites Ajouter supprimer modifier

---
+++ src/Form/SiteType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Site;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType:: class, [
+                'attr' => ['placeholder' => 'Nom de la ville'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Site::class,
+            'method' => 'POST',
+            'csrf_protection' => true,
+        ]);
+    }
+}
