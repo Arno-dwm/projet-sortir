c93bae02c87855f881cd9c14f30aa6ea09073fc5
Arno <arnaud.douillard2025@campus-eni.fr>
2026-03-04 11:28:59 +0100

    ajout lieu et autocomplete OK

---
+++ src/Controller/LieuController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Lieu;
+use App\Form\LieuType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LieuController extends AbstractController
+{
+    #[Route('/lieu/create', name: 'app_lieu_create')]
+    public function creerLieu(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $lieu = new Lieu();
+        $form = $this->createForm(LieuType::class, $lieu);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($lieu);
+            $entityManager->flush();
+
+            //Pour rafraichir la liste des lieux dans le form création sortie
+            $lieux = $entityManager->getRepository(Lieu::class)->findAll();
+            //Utilisation de Turbo Stream pour conserver les infos sur le form création Sortie
+            return $this->render('lieu/create.stream.html.twig', [
+                'lieu' => $lieu,
+                'lieux' => $lieux,
+            ],new Response('', 200, [
+                'Content-Type' => 'text/vnd.turbo-stream.html'
+            ]));
+
+        }
+
+        return $this->render('parts/_form-lieu.html.twig', [
+            'formlieu' => $form,
+        ]);
+    }
+
+    #[Route('/lieu/{id}/json', name: 'lieu_json', methods: ['GET'])]
+    public function lieuJson(Lieu $lieu): JsonResponse
+    {
+        return $this->json([
+            'rue' => $lieu->getRue(),
+            'latitude' => $lieu->getLatitude(),
+            'longitude' => $lieu->getLongitude(),
+            'codePostal' => $lieu->getVille()->getCodePostal(),
+        ]);
+    }
+}
